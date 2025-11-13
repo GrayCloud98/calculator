57efa8cdaab415ddb3e059e29a276c8f2be12f70
Ziad Morsy <ziad.morsy1998@hotmail.com>
2025-11-13 22:01:03 +0100

    react native somehow works

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,17 +1,25 @@
 const path = require('path');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '../..');
 
-module.exports = {
+const config = {
   projectRoot,
   watchFolders: [
+    workspaceRoot,
     path.resolve(workspaceRoot, 'packages/shared'),
   ],
+
   resolver: {
+    unstable_enableSymlinks: true,
+    unstable_enablePackageExports: true,
+
     nodeModulesPaths: [
-      path.resolve(projectRoot, 'node_modules'),
-      path.resolve(workspaceRoot, 'node_modules'),
+      path.join(projectRoot, 'node_modules'),
+      path.join(workspaceRoot, 'node_modules'),
     ],
   },
 };
+
+module.exports = mergeConfig(getDefaultConfig(projectRoot), config);
